4a598666850b9e30afc96102d2d3207c639814bc
Fatih Şallı <mimsallifatih@gmail.com>
2023-01-10 20:11:37 +0300

    .

--- Presentation/ECommerceAPI.API/Program.cs
+++ Presentation/ECommerceAPI.API/Program.cs
@@ -1,8 +1,3 @@
-
-using ECommerceAPI.Persistence;
-
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -10,7 +5,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddPersistenceServices();
 
 
 var app = builder.Build();
