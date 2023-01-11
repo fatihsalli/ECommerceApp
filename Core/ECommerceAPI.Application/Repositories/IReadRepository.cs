d6dd9178e38b5fb55b2eb7561c5318033126b4fc
Fatih Şallı <mimsallifatih@gmail.com>
2023-01-11 11:14:50 +0300

    IRepository

--- Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
+++ Core/ECommerceAPI.Application/Repositories/IReadRepository.cs
@@ -14,7 +14,7 @@ namespace ECommerceAPI.Application.Repositories
 
         //Verilen şart ifadesi doğru olan dataların (<T,bool>) çekileceği anlamına gelir. x=> x.Name==name gibi ifadeler özünde Expression olduğu için bu şekilde sorgulama yapabiliyoruz.
         IQueryable<T> GetWhere(Expression<Func<T,bool>> method);
-        T GetSingle(Expression<Func<T, bool>> method);
-        T GetById(string id);
+        Task<T> GetSingleAsync(Expression<Func<T, bool>> method);
+        Task<T> GetByIdAsync(string id);
     }
 }
--- Core/ECommerceAPI.Application/Repositories/IWriteRepository.cs
+++ Core/ECommerceAPI.Application/Repositories/IWriteRepository.cs
@@ -8,7 +8,11 @@ namespace ECommerceAPI.Application.Repositories
 {
     public interface IWriteRepository<T> : IRepository<T> where T : class
     {
-        
+        Task<bool> AddAsync(T model);
+        Task<bool> AddAsync(List<T> model);
+        Task<bool> UpdateAsync(T model);
+        bool Remove(T model);
+        bool Remove(string id);
 
     }
 }
